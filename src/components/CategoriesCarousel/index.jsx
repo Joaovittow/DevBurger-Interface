35c9ceb0c40d2d560633654f417dd9d04de1b8af
Joaovittow <joaovitorseixasn13@outlook.com>
2025-04-17 17:00:01 -0300

    feat(categoriesCarousel): Add `react-multi-carousel` for a responsive categories display with styled components

--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
+import Carousel from 'react-multi-carousel';
 import { api } from '../../services/api';
+
+import 'react-multi-carousel/lib/styles.css';
+
+import { Container, ContainerItems, Title } from './styles';
 
 export function CategoriesCarousel() {
   const [categories, setCategories] = useState([]);
@@ -9,13 +14,45 @@ export function CategoriesCarousel() {
       const { data } = await api.get('/categories');
 
       setCategories(data);
-      console.log(data);
     }
     loadCategories();
   }, []);
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1280 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1280, min: 690 },
+      items: 3,
+    },
+    mobile: {
+      breakpoint: { max: 690, min: 0 },
+      items: 2,
+    },
+  };
+
   return (
-    <div>
-      <h1>CategoryCarousel</h1>
-    </div>
+    <Container>
+      <Title>Categorias</Title>
+
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+        partialVisible={false}
+        itemClass="carousel-item"
+      >
+        {categories.map((category) => (
+          <ContainerItems key={category.id} imageUrl={category.url}>
+            <p>{category.name}</p>
+          </ContainerItems>
+        ))}
+      </Carousel>
+    </Container>
   );
 }
