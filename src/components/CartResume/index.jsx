ad6213052e4e735a13da88c123df12fd64b7d12a
joaovittow <joaovitorseixasn13@outlook.com>
2025-08-21 11:18:31 -0300

    feat(cart/resume): Add dynamic calculation of total price in `CartResume` component using `useEffect` and `useState` hooks

--- src/components/CartResume/index.jsx
+++ src/components/CartResume/index.jsx
@@ -1,21 +1,41 @@
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { useCart } from '../../hooks/CartContext';
+import { api } from '../../services/api';
+
+import { formatPrice } from '../../utils/formatPrice';
+
 import { Button } from '../Button';
 import { Container } from './styles';
 
 export function CartResume() {
+  const [finalPrice, setFinalPrice] = useState(0);
+  const [deliveryTax] = useState(500);
+
+  const { cartProducts } = useCart();
+
+  useEffect(() => {
+    const sumAllItems = cartProducts.reduce((acc, current) => {
+      return current.price * current.quantity + acc;
+    }, 0);
+
+    setFinalPrice(sumAllItems);
+  }, [cartProducts]);
+
   return (
     <div>
       <Container>
         <div className="container-top">
           <h2 className="title">Resumo do Pedido</h2>
           <p className="items">Itens</p>
-          <p className="items-price">R$ 20,00</p>
+          <p className="items-price">{formatPrice(finalPrice)}</p>
           <p className="delivery-tax">Taxa de entrega</p>
-          <p className="delivery-tax-price">R$ 5,00</p>
+          <p className="delivery-tax-price">{formatPrice(deliveryTax)}</p>
         </div>
 
         <div className="container-bottom">
           <p>Total</p>
-          <p>R$ 25,00</p>
+          <p>{formatPrice(finalPrice + deliveryTax)}</p>
         </div>
       </Container>
       <Button>Finalizar Pedido</Button>
