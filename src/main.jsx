6d22e04bc63724884c558a8879177922f8d77b39
joaovittow <joaovitorseixasn13@outlook.com>
2025-10-29 13:03:09 -0300

    feat(layout/user): Add `UserLayout` component to wrap routes with `Header` and `Footer`

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
 
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import { Elements } from '@stripe/react-stripe-js';
 import { ThemeProvider } from 'styled-components';
 import stripePromise from './config/stripeConfig.js';
 import AppProvider from './hooks/index.jsx';
-import { router } from './routes/index.jsx';
+import { Router } from './routes';
 import GlobalStyle from './styles/globalStyles.js';
 import { standardTheme } from './styles/themes/standard.js';
 
@@ -17,7 +17,9 @@ createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={standardTheme}>
       <AppProvider>
         <Elements stripe={stripePromise}>
-          <RouterProvider router={router} />
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
         </Elements>
         <GlobalStyle />
         <ToastContainer autoClose={2000} theme="colored" />
