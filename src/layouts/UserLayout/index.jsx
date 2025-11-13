555ef6fd45560d1cdf3c4169828d852e5a940681
joaovittow <joaovitorseixasn13@outlook.com>
2025-11-13 09:44:26 -0300

    fix(auth): Redirect to login with state if user is not authenticated in `UserLayout`

--- src/layouts/UserLayout/index.jsx
+++ src/layouts/UserLayout/index.jsx
@@ -2,22 +2,17 @@ import { Outlet, Navigate, useLocation } from 'react-router-dom';
 import { Footer, Header } from '../../components';
 import { useAuth } from '../../hooks/useAuth';
 
-// Rotas que precisam de autenticação
-const protectedRoutes = ['/checkout', '/complete'];
-
 export function UserLayout() {
   const { isAuthenticated, isLoading } = useAuth();
   const location = useLocation();
-  const isProtectedRoute = protectedRoutes.includes(location.pathname);
 
   // Mostrar loading enquanto verifica autenticação
   if (isLoading) {
     return null; // Ou um componente de loading
   }
 
-  // Se a rota precisa de autenticação e o usuário não está logado, redirecionar
-  if (isProtectedRoute && !isAuthenticated) {
-    return <Navigate to="/login" />;
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace state={{ from: location.pathname }} />;
   }
 
   return (
